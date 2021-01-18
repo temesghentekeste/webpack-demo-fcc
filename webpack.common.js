156f7731029c220fe11c0b02f118ce48780127d4
temesghen tekeste <mail.temesghen.tekeste@gmail.com>
2021-01-18 18:26:39 +0300

    Minify JS, CSS, and HTML in production
    - Extracted CSS into own file in production
    - Minified CSS in production
    - Added TerserJS back in to minify JS in production
    - Minified HTML in production as well

--- webpack.common.js
+++ webpack.common.js
@@ -1,5 +1,4 @@
 const path = require('path');
-const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
   entry: {
@@ -7,21 +6,9 @@ module.exports = {
     vendor: path.resolve(__dirname, './src/vendor.js'),
   },
 
-  plugins: [
-    new HtmlWebpackPlugin({
-      template: './src/template.html',
-    }),
-  ],
   module: {
     rules: [
-      {
-        test: /\.scss$/,
-        use: [
-          'style-loader', //3. Inject styles into DOM
-          'css-loader', //2. Turns css into commonjs
-          'sass-loader', //1. Turns sass into css
-        ],
-      },
+      
       {
         test: /\.html$/,
         use: ['html-loader'],
